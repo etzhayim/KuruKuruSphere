78adbd4a52ff485c228056e91154e35ac0078657
横山聡 <etzhayim@mbe.nifty.com>
2017-10-29 07:53:03 +0900

    自作ゲームプロジェクト　作成途中のコミット
    自作ゲーム用にGameSceneを作ったファイルです。

---
+++ Assets/DiskController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiskController : MonoBehaviour {
+
+    // 回転速度
+    private float rotSpeed = 1.0f;
+    Rigidbody rg;
+
+    // Use this for initialization
+    void Start () {
+        rg = GetComponent<Rigidbody>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // 回転
+        //this.transform.Rotate(0, this.rotSpeed, 0);
+        rg.angularVelocity = new Vector3(0, this.rotSpeed, 0);
+    }
+
+}
---
+++ Assets/SphereController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereController : MonoBehaviour {
+
+    private Rigidbody myRigidbody;
+    private float moveForce = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+        this.myRigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.myRigidbody.AddForce(moveForce, 0, 0, ForceMode.Acceleration);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.myRigidbody.AddForce(-moveForce, 0, 0, ForceMode.Acceleration);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.myRigidbody.AddForce(0, 0, moveForce, ForceMode.Acceleration);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.myRigidbody.AddForce(0, 0, -moveForce, ForceMode.Acceleration);
+        }
+    }
+    
+    void OnCollisionEnter(Collision other)
+    {
+        if (transform.parent == null && other.gameObject.tag == "MoveStage")
+        {
+            var emptyObject = new GameObject();
+            emptyObject.transform.parent = other.gameObject.transform;
+            transform.parent = emptyObject.transform;
+        }
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        if (transform.parent != null && other.gameObject.tag == "MoveStage")
+        {
+            transform.parent = null;
+        }
+    }
+    
+}
